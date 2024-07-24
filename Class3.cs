23aa9502e24a02340587bc05bd66bbcd1e40f375
Eu sejo qualquer coisa <114186739+SagDig@users.noreply.github.com>
2024-07-24 11:18:34 -0300

    Add files via upload

---
+++ Class3.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+int num;
+
+Console.WriteLine("Digite um número de 1 a 10");
+Convert.ToInt32(Console.ReadLine(nun));
+
+switch (nun) 
+{
+    case 1:
+        Console.WriteLine("Pirarucu");
+        break;
+
+    case 2:
+        Console.WriteLine("Piranha");
+        break;
+
+    case 3:
+        Console.WriteLine("Robalo");
+        break;
+
+    case 4:
+        Console.WriteLine("Sardinha");
+        break;
+
+    case 5:
+        Console.WriteLine("Truta");
+        break;
+
+    case 6:
+        Console.WriteLine("Atum");
+        break;
+
+    case 7:
+        Console.WriteLine("Caranguejo");
+        break;
+
+    case 8:
+        Console.WriteLine("Coelho do mar");
+        break;
+
+    case 9:
+        Console.WriteLine("Baleia");
+        break;
+
+    case 10:
+        Console.WriteLine("Diabolik Lovers!!");
+        break;
+}
+
